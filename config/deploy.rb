e169602ccc71fb8d42b4c58a1e848b1b2dc06d0a
code-R <vamsi.skrishna@gmail.com>
2014-09-25 13:47:49 +0530

    adding bootstrap helper

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,6 +3,7 @@ lock '3.2.1'
 
 set :application, 'filmy'
 set :repo_url, 'git@github.com:bhaag-code-bhaag/filmy.git'
+set :branch, 'add-movies'
 
 # set :domain, '54.69.83.161'
 
@@ -30,7 +31,7 @@ set :deploy_to, '/var/www/filmy'
 set :linked_files, %w{config/database.yml config/secrets.yml}
 
 # Default value for linked_dirs is []
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{public/uploads}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
@@ -42,9 +43,13 @@ namespace :deploy do
 
   desc 'Restart application'
   task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
+    # on roles(:app), in: :sequence, wait: 5 do
+    #   # Your restart mechanism here, for example:
+    #   execute :touch, release_path.join('tmp/restart.txt')
+    # end
+    on roles(:web), in: :sequence, wait: 5 do
+      execute :mkdir, '-p', "#{ release_path }/tmp"
+      execute :touch, release_path.join('tmp/restart.txt')
     end
   end
 
